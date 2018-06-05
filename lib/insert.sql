cc52b205af46a640889b66d01b9922cd10674b40
Grace Shih <vocalise@gmail.com>
2018-06-05 01:50:04 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -19,4 +19,4 @@ INSERT INTO users(name, age) VALUES("Kailyn", 42);
 INSERT INTO users(name, age) VALUES("Happy", 35);
 INSERT INTO users(name, age) VALUES("Dr Dre", 36);
 
-INSERT INTO projects VALUES ("")
+INSERT INTO projects VALUES ("Travel Gear", 30000.00, "January 6, 2018", "January 31, 2018");
