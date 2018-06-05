c4d9938083746ebe59735fdcee6131864d176321
Grace Shih <vocalise@gmail.com>
2018-06-05 01:47:19 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -22,24 +22,4 @@ CREATE TABLE pledges (
 
 
 
-INSERT INTO users(name, age) VALUES("Valerie", 26);
-INSERT INTO users(name, age) VALUES("Robert", 28);
-INSERT INTO users(name, age) VALUES("Mary", 22);
-INSERT INTO users(name, age) VALUES("Josephine", 40);
-INSERT INTO users(name, age) VALUES("Alan", 42);
-INSERT INTO users(name, age) VALUES("Ming", 43);
-INSERT INTO users(name, age) VALUES("Katya", 50);
-INSERT INTO users(name, age) VALUES("Win", 44);
-INSERT INTO users(name, age) VALUES("Adam", 44);
-INSERT INTO users(name, age) VALUES("Michael", 37);
-INSERT INTO users(name, age) VALUES("Gregory", 34);
-INSERT INTO users(name, age) VALUES("Henry", 35);
-INSERT INTO users(name, age) VALUES("Sean", 26);
-INSERT INTO users(name, age) VALUES("Levine", 25);
-INSERT INTO users(name, age) VALUES("Cartman", 24);
-INSERT INTO users(name, age) VALUES("Butters", 24);
-INSERT INTO users(name, age) VALUES("Kevin", 33);
-INSERT INTO users(name, age) VALUES("Kailyn", 42);
-INSERT INTO users(name, age) VALUES("Happy", 35);
-INSERT INTO users(name, age) VALUES("Dr Dre", 36);
-INSERT INTO users(name, age) VALUES("Aron", 36);
+
