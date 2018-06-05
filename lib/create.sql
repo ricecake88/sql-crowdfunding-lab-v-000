58deb5dec70956869bc9c9e58e84a1b6645666c4
Grace Shih <vocalise@gmail.com>
2018-06-05 01:38:59 +0000

    automatically backed up by learn

---
+++ lib/create.sql
@@ -0,0 +1,20 @@
+CREATE TABLE projects (
+  project_id INTEGER PRIMARY KEY,
+  title TEXT,
+  category TEXT,
+  funding_goal REAL,
+  start_date TEXT,
+  end_date TEXT
+)
+
+CREATE TABLE users (
+  user_id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER
+)
+
+CREATE TABLE pledges (
+  user_id INTEGER,
+  project_id INTEGER,
+  amount REAL
+)
