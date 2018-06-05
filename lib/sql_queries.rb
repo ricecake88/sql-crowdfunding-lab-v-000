82d328fe7cfc8f3df6ef6cc65931ab7299bfdc52
Grace Shih <vocalise@gmail.com>
2018-06-05 03:51:56 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,7 +15,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"sqlite> SELECT users.name, users.age, SUM(pledges.amount) AS amount FROM users INNER JOIN pledges ON pledges.user_id = users.id GROUP BY(users.name) ORDER BY users.name, amount;"
+"SELECT users.name, users.age, SUM(pledges.amount) AS amount FROM users INNER JOIN pledges ON pledges.user_id = users.id GROUP BY(users.name) ORDER BY users.name, amount;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
