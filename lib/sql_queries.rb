cf800ba0b0c925c3fb8497647fd7f6f4e74c9b19
Grace Shih <vocalise@gmail.com>
2018-06-05 03:51:09 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -14,12 +14,12 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 "SELECT users.age, users.name, SUM(pledges.amount) FROM users INNER JOIN pledges ON pledges.user_id = users.id GROUP BY users.name;"
 end
 
-def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT projects.title, projects.funding_goal-SUM(pledges.amount) AS fund_difference,  FROM projects INNER JOIN pledges ON projects.id = pledges.project_id GROUP BY projects.title HAVING fund_difference <= 0;"
-end
-
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
 "sqlite> SELECT users.name, users.age, SUM(pledges.amount) AS amount FROM users INNER JOIN pledges ON pledges.user_id = users.id GROUP BY(users.name) ORDER BY users.name, amount;"
+end
+
+def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
+"SELECT projects.title, projects.funding_goal-SUM(pledges.amount) AS fund_difference,  FROM projects INNER JOIN pledges ON projects.id = pledges.project_id GROUP BY projects.title HAVING fund_difference <= 0;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
