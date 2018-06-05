a51a6a5a7a065e2ec092eb1bd22886dcce6c9dbe
Grace Shih <vocalise@gmail.com>
2018-06-05 03:53:07 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,11 +15,11 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT projects.title, projects.funding_goal-SUM(pledges.amount) AS fund_difference,  FROM projects INNER JOIN pledges ON projects.id = pledges.project_id GROUP BY projects.title HAVING fund_difference <= 0;"
+"SELECT projects.title, projects.funding_goal-SUM(pledges.amount) AS fund_difference  FROM projects INNER JOIN pledges ON projects.id = pledges.project_id GROUP BY projects.title HAVING fund_difference <= 0;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"SELECT users.name, users.age, SUM(pledges.amount) AS amount FROM users INNER JOIN pledges ON pledges.user_id = users.id GROUP BY(users.name) ORDER BY users.name, amount;"
+""
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
